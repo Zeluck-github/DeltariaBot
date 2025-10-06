7ec82f1fcd1e900b9f39a51d2f9756c8360adb6d
Zeluck-github <nonocreatif@gmail.com>
2025-10-06 22:37:56 +0200

    Add message delete and upgrade logging events

---
+++ src/events/logs/message/message_upgrade.ts
@@ -0,0 +1,25 @@
+import type { GuildChannel } from "discord.js"
+import { EmbedBuilder} from "discord.js"
+
+import type CustomClient from "../../../structures/CustomClient.js"
+import clientConfig from "../../../structures/config.js"
+import { lb } from "../../../utils/lb.js"
+
+export default {
+    name: "messageUpgrade",
+    once: false,
+    async execute(client: CustomClient, channel: GuildChannel) {
+
+        const embed = new EmbedBuilder()
+        //
+        // Embed content
+        //
+
+        const logChannel = channel.guild.channels.cache.get(clientConfig.LOG_CHANNEL_ID)
+        if (logChannel?.isTextBased()) {
+            await logChannel.send({
+                embeds: [embed],
+            })
+        }
+    },
+} 
